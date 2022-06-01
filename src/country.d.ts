84d597c618e6c370adc1c635e91955389c2ecdb4
Dante Benicio <dantecosta79@gmail.com>
2022-06-01 18:32:02 -0300

    CountryInformations types definition file

---
+++ src/country.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+type CountryInformations = {
+  capital: string
+  countryName: string
+  population: number
+  region: string
+  flags: {
+    png: string
+    svg: string
+  }
+}
