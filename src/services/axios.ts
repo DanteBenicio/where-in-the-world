7afc241fed67c650eba1941a5c0aab30c98aa15c
Dante Benicio <dantecosta79@gmail.com>
2022-06-04 23:34:05 -0300

    create countriesRequest axios instance

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:3000/api',
+})
+
+export const countriesRequest = axios.create({
+  baseURL: process.env.COUNTRIES_API_URL,
 })
