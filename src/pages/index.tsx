6810c9b82ba415a76d8709b4617353fa73e4e3d0
Dante Benicio <dantecosta79@gmail.com>
2022-06-04 23:31:45 -0300

    remove code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,34 +29,7 @@ export const getServerSideProps: GetServerSideProps<{
   countriesData?: CountryInformations[]
 }> = async () => {
   try {
-    const { data: countriesData } = await axios.get(
-      'https://restcountries.com/v3.1/all',
-      {
-        transformResponse: [
-          (data) => {
-            const parsedData = JSON.parse(data)
-            const countries = []
-
-            for (let i = 0; i < 10; i++) {
-              const country = {
-                capital: parsedData[i]?.capital?.[0] || null,
-                countryName: parsedData[i]?.name?.common,
-                population: parsedData[i]?.population,
-                region: parsedData[i]?.region,
-                flags: {
-                  png: parsedData[i]?.flags.png,
-                  svg: parsedData[i]?.flags.svg,
-                },
-              }
-
-              countries.push(country)
-            }
-
-            return countries
-          },
-        ],
-      },
-    )
+    const { data: countriesData } = await api.get('/getAllCountries')
 
     return {
       props: { countriesData },
