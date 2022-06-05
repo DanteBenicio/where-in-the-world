55aeed353313d23c9af099f43a89c0d4d55f9546
Dante Benicio <dantecosta79@gmail.com>
2022-06-04 23:33:36 -0300

    add more propertys

--- src/country.d.ts
+++ src/country.d.ts
@@ -4,6 +4,23 @@ type CountryInformations = {
   countryName: string
   population: number
   region: string
+  sub_region: string
+  top_level_domain: string[]
+  currencies: {
+    [key: string]: {
+      name: string
+      symbol: string
+    }
+  }
+  nativeName: {
+    [key: string]: {
+      official: string
+      common: string
+    }
+  }
+  languages: {
+    [key: string]: string
+  }
   flags: {
     png: string
     svg: string
