80e3b49bc80fec71218da37b018836dff0295d6a
Dante Benicio <dantecosta79@gmail.com>
2022-06-07 18:38:24 -0300

    render Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ContextProvider } from '../context'
+import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ContextProvider>
+      <Header />
       <Component {...pageProps} />
     </ContextProvider>
   )
