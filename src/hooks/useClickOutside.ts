7aeabf39c5d41ca3b1db1ecec5f4bd25e60e0bbd
Dante Benicio <dantecosta79@gmail.com>
2022-06-15 19:05:23 -0300

    create useClickOutside hook

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+export function useClickOutside(
+  handler: () => void, 
+  elementRef: React.MutableRefObject<any>, 
+  ignoredElement?: React.MutableRefObject<any>
+): React.MutableRefObject<any> {
+  useEffect(() => {
+    document.addEventListener('click', e => {
+      const clickedElement = e.target as HTMLElement
+
+      if (ignoredElement) {
+        if (!elementRef.current?.contains(clickedElement) && clickedElement !== ignoredElement.current) {
+          handler()
+        }
+
+        return elementRef.current
+      }
+
+      if (!elementRef.current?.contains(clickedElement)) {
+        handler()
+      }
+    })
+
+    return () => document.removeEventListener('click', () => {})
+  }, [])
+
+  return elementRef.current
+}
