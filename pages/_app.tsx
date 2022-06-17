fa7b61f73084369e2d3318dfb81f49372f44e8bb
Dante Benicio <dantecosta79@gmail.com>
2022-06-16 22:29:14 -0300

    add fonts links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,23 @@ import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ContextProvider } from '../src/context'
 import Header from '../src/components/Header'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ContextProvider>
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin=""
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap"
+        rel="stylesheet"
+      />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </ContextProvider>
