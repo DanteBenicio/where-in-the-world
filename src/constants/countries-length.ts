e2e2967fd90e8c75aef7ecd9a1a568a260e58272
Dante Benicio <dantecosta79@gmail.com>
2022-06-20 19:59:49 -0300

    constants folder

---
+++ src/constants/countries-length.ts
@@ -0,0 +1,5 @@
+export const ALL_AMERICAS_COUNTRIES = 56
+export const ALL_EUROPE_COUNTRIES = 53
+export const ALL_AFRICA_COUNTRIES = 59
+export const ALL_ASIA_COUNTRIES = 50
+export const ALL_OCEANIA_COUNTRIES = 27
